827955f85fefc528e72b16cac2f7a2ffb3ba0a46
SANGRAM-LOHOKARE <sangramlohokare26@gmail.com>
2026-06-24 14:13:02 +0530

    object recog added

--- My-First-Project-1/README.dataset.txt
+++
@@ -1,6 +0,0 @@
-# My First Project > 2026-06-20 12:13pm
-https://universe.roboflow.com/sangrams-workspace-awhwb/my-first-project-1tse1
-
-Provided by a Roboflow user
-License: CC BY 4.0
-
--- object_detection/train.py
+++ object_detection/train.py
@@ -4,7 +4,8 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
     # 1. PASTE YOUR EXACT ROBOFLOW CODE HERE TO AUTO-DOWNLOAD DATA
-    rf = Roboflow(api_key="fokAKZ95uWIN4CRw4sY3")
+    key = os.getenv("ROBOFLOW_API_KEY")
+    rf = Roboflow(api_key=key)
     project = rf.workspace("sangrams-workspace-awhwb").project("my-first-project-1tse1")
     version = project.version(1)
     dataset = version.download("yolov8")
@@ -12,18 +13,16 @@ if __name__ == '__main__':
     # 2. Get the path to the downloaded data.yaml file
     yaml_path = os.path.join(dataset.location, "data.yaml")
 
-    # 3. Load a lightweight YOLOv8 Nano model skeleton
+    # 3. Load a YOLOv8 Nano model
     model = YOLO("yolov8n.pt") 
 
     # 4. Start the training process
-    # epochs=30 tells the model to study your 15 images 30 times over
     model.train(
         data=yaml_path, 
-        epochs=30, 
+        epochs=30,        # epochs=30 tells the model to study your 15 images 30 times over
         imgsz=640, 
         device="cpu",
-        workers=0,        # Keeps it stable on Windows CPU
+        workers=0,
         batch=4           # Prevents memory overload
 )
-    
     print("Training finished! Your custom weights are ready at: runs/detect/train/weights/best.pt")
